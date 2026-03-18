ecda2538e7bb23d3935c7fe3bc17964cbf1896d0
ana ribeiro <anadssribeiro@icloud.com>
2026-03-18 16:19:34 +0100

    scanpage initial setup

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,9 @@
 class MessagesController < ApplicationController
-  has_one_attached :file
 end
 
-ruby_llm_chat = RubyLLM.chat(model: "gpt-4o") # vision-capable model
-prompt = "Could you analyse the error message \
-  on the screenshot and help me solve it?"
-response = ruby_llm_chat.ask(prompt, with: {image: "tmp/Screenshot.png"})
-puts response.content
-puts "Tokens used: #{response.input_tokens} input, #{response.output_tokens} output"
+# ruby_llm_chat = RubyLLM.chat(model: "gpt-4o") # vision-capable model
+# prompt = "Could you analyse the error message \
+#on the screenshot and help me solve it?"
+# response = ruby_llm_chat.ask(prompt, with: {image: "tmp/Screenshot.png"})
+# puts response.content
+# puts "Tokens used: #{response.input_tokens} input, #{response.output_tokens} output"

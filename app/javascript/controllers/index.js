6ac5aec1fddb2c89371cbf9d69f34d917f9bbbdb
ana ribeiro <anadssribeiro@icloud.com>
2026-03-18 20:42:42 +0100

    scanning

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+import CameraController from "./camera_controller"
+import FormValidationController from "./form_validation_controller"
+
+application.register("camera", CameraController)

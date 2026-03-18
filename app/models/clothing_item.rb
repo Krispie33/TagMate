65ae5522f933c32e1db71ab7327430e7bd2e4d16
ana ribeiro <anadssribeiro@icloud.com>
2026-03-18 16:54:28 +0100

    scan setup without form and camera

--- app/models/clothing_item.rb
+++ app/models/clothing_item.rb
@@ -3,4 +3,7 @@ class ClothingItem < ApplicationRecord
 
   has_one_attached :tag_image
   has_one_attached :item_image
+
+  validates :tag_image, presence: true
+  validates :item_image, presence: true
 end

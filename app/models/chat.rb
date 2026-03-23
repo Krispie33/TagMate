cd81df6434c8ccd9f77c0e4c5a513e3b4c466933
Prasanna Sivabalan <spshanna2010@gmail.com>
2026-03-23 13:05:26 +0100

    chats done and some designing as well

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -13,12 +13,10 @@ class Chat < ApplicationRecord
   # validates :title, presence: true
 
   def generate_title_from_first_message
-    # return unless title == DEFAULT_TITLE
+    first_user_message = messages.where(role: "user").order(:created_at).first
+    return if first_user_message.nil?
 
-    # first_user_message = messages.where(role: "user").order(:created_at).first
-    # return if first_user_message.nil?
-
-    response = RubyLLM.chat.with_instructions(TITLE_PROMPT).ask(drawer)
+    response = RubyLLM.chat.with_instructions(TITLE_PROMPT).ask(first_user_message.content)
     update(title: response.content)
   end
 end

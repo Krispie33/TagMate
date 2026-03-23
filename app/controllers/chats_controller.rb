c0f627498431b68140914a19d29e5c41a4150bec
Kris Lefebure <krislefebure@hotmail.com>
2026-03-23 12:06:18 +0100

    Ask Andrew

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,13 @@
 class ChatsController < ApplicationController
   def create
-    @clothing_item = current_user.clothing_items.find(params[:clothing_item_id])
-    @chat = Chat.new(user: current_user, drawer: @clothing_item.drawer)
+    @drawer = current_user.drawers.find(params[:drawer_id])
 
-    if @chat.save
+    @chat = Chat.new(user: current_user, drawer: @drawer)
+    @chat.title = @drawer.name
+    if @chat.save!
       redirect_to chat_path(@chat)
     else
-      render "clothing_items/show", status: :unprocessable_entity
+      redirect_to drawer_path(@drawer), alert: "Could not start a chat. Please try again."
     end
   end
 
